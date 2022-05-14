d17845d97cb8d6ac585f8d3f49cbebcbeb7f1fa5
buildingsoftwareblocks <buildingsoftwareblocks@gmail.com>
2022-05-14 15:20:30 +0200

    init

---
+++ src/main/java/com/btb/rdfe/Rdf4jEscoApplication.java
@@ -0,0 +1,13 @@
+package com.btb.rdfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rdf4jEscoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rdf4jEscoApplication.class, args);
+    }
+
+}
